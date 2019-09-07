3d57b8dd203fe3b89a87295bfc5a2715325b631c
Jadsia <megankyoung@live.com>
2019-09-07 15:03:08 -0700

    updated test language still not complete

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe RecipesController, type: :controller do
-
-
+ describe "recipes#index action" do
+  it "should show the recipes created in the databse"
+ # @recipes = Recipe.all
+  get :index
+  expect(recipes).not_to be_empty
+ end
 end
--- spec/controllers/searchrecipe_controller_spec.rb
+++ spec/controllers/searchrecipe_controller_spec.rb
@@ -2,20 +2,17 @@ require 'rails_helper'
 
 RSpec.describe SearchrecipeController, type: :controller do
 
+    describe "searchByName" do
+      it "goes to /search and enters cranberries, hits search and show results"
+    expect(response).to have_http_status(:success)
+   end
 
-#   describe SearchrecipeController do
-#     describe "searchByName" do
-#       it "goes to /searchByName and enters cranberries, hits search"
-#       it "shows results"
-#     #expect(response.status).to eq(200)
-#     #expect(response).to have_http_status(:success)
-#   end
-# end
 
-describe "get about page"
-it "renders the :about view" do
-  get :about
-  response.should render_template :about
+# THIS TEST is written correctly, but route is not on searchrecipe controller
+ describe "get about page"
+ it "renders the :about view" do
+   get :about
+   response.should render_template :about
 end
 
 
