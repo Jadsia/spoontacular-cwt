05970d2fb60423a9c0e0e504a38c6a6b501a6966
Jadsia <megankyoung@live.com>
2019-09-07 17:54:34 -0700

    added working index test and nonworking user log in test

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe RecipesController, type: :controller do
- describe "recipes#index action" do
-  it "should show the recipes created in the databse"
- # @recipes = Recipe.all
-  get :index
-  expect(recipes).not_to be_empty
- end
+  describe "GET #index" do
+    before do
+      get :index
+    end
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'Authentication' do
+    it 'should allow users to log in' do
+      user = FactoryBot.create(:user, password: 'password')
+      get new_sessions_url
+      fill_in 'email', with: user.email
+      fill_in 'password', with: 'password'
+      click_button 'Login'
+
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+
+
 end
--- spec/controllers/searchrecipe_controller_spec.rb
+++ spec/controllers/searchrecipe_controller_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe SearchrecipeController, type: :controller do
 
-    describe "searchByName" do
-      it "goes to /search and enters cranberries, hits search and show results"
-    expect(response).to have_http_status(:success)
-   end
+  #   describe "searchByName" do
+  #     it "goes to /search and enters cranberries, hits search and show results"
+  #   expect(response).to have_http_status(:success)
+  #  end
 
 
 # THIS TEST is written correctly, but route is not on searchrecipe controller
- describe "get about page"
- it "renders the :about view" do
-   get :about
-   response.should render_template :about
-end
+#  describe "get about page"
+#  it "renders the :about view" do
+#    get :about
+#    response.should render_template :about
+# end
 
 
 end
